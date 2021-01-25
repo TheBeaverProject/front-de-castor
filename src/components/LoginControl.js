66024586324198319c7ed4bedc7c99ab3f8d521f
francois.houssin <francois1.houssin@epita.fr>
2021-01-25 16:40:44 +0100

    petit coup d'eslint pour le style

--- src/components/LoginControl.js
+++ src/components/LoginControl.js
@@ -1,35 +1,35 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 const LoginControl = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    // Check if user is logged in
     /**
+     * Check if user is logged in
      * From beaver api: /auth/check
      * @returns valid: bool
      */
     fetch("http://151.80.152.11/auth/check")
-    .then(res => setIsLoggedIn(res.valid))
-    // Token verification failed
-    // Kill the backend dev
-    .catch(err => console.error(err))
-  })
+      .then((res) => setIsLoggedIn(res.valid))
+      // Token verification failed
+      // Kill the backend dev
+      .catch((err) => console.error(err));
+  });
 
-  if(isLoggedIn){
+  if (isLoggedIn) {
     return (
       <div>
         <button href="#dashboard">Dashboard</button>
       </div>
-    )
+    );
   } else {
     return (
       <div>
         <button href="#login">Login</button>
         <button href="#register">Register</button>
       </div>
-    )
+    );
   }
-}
+};
 
-export default LoginControl
+export default LoginControl;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,22 @@ import React from "react";
 import LoginControl from "./LoginControl";
 
 const NavBar = () => {
-  
   return (
     <nav>
       <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li><a href="#support">Support us</a></li>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+        <li>
+          <a href="#support">Support us</a>
+        </li>
       </ul>
       <LoginControl />
     </nav>
-  )
+  );
 };
 
 export default NavBar;
