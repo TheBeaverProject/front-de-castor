ff8f52d954d998d95329cdaef7da1c6bcdf7886d
francois.houssin <francois1.houssin@epita.fr>
2021-01-26 13:47:52 +0100

    add retards contact

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -10,9 +10,24 @@ const Contact = () => {
         <div className="main-content">
           <div className="contact-container">
               <article>
-                  <h3>François Houssin</h3>
+                  <h3>François HOUSSIN</h3>
                   <p>fransoishoussin@gmail.com</p>
                   <p>github.com/Difrancium/</p>
+              </article>
+              <article>
+                  <h3>Augustin BEGUE</h3>
+                  <p>augustin.begue@epita.fr</p>
+                  <p>github.com/Tagueo</p>
+              </article>
+              <article>
+                  <h3>Baptiste FONTAINE</h3>
+                  <p>baptiste.fontaine@epita.fr</p>
+                  <p>github.com/Hypaxba</p>
+              </article>
+              <article>
+                  <h3>Paul SPIELMANN</h3>
+                  <p>paul.spielmann@epita.fr</p>
+                  <p>github.com/paul-spl</p>
               </article>
           </div>
         </div>
