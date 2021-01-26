aa4697dee823d473eba5d5d47a458382a18d5f98
francois.houssin <francois1.houssin@epita.fr>
2021-01-26 19:39:15 +0100

    link now push in webapp history instead of browser history (yeah it's pur stonking ^^)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
+import {useHistory} from 'react-router-dom'
+
 import LoginControl from "./LoginControl";
 
 const NavBar = () => {
+  const history = useHistory()
   return (
     <nav>
       <ul>
         <li>
-          <Link to="/home">Home</Link>
+          <button onClick={() => history.push("/home")}>Home</button>
         </li>
         <li>
-          <Link to="/news">News</Link>
+          <button onClick={() => history.push("/news")}>News</button>
         </li>
         <li>
-          <Link to="/contact">Contact</Link>
+          <button onClick={() => history.push("/contact")}>Contact</button>
         </li>
         <li>
-          <Link to="/support">Support us</Link>
+          <button onClick={() => history.push("/supportus")}>Support us</button>
         </li>
       </ul>
       <LoginControl />
