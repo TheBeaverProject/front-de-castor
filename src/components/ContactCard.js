4fbfb3dd7344eb00bf2b6bda613857ab51eb9605
Simon Scatton <simon.scatton@epita.fr>
2021-01-28 20:35:57 +0100

    Finished implementing contact forms

---
+++ src/components/ContactCard.js
@@ -0,0 +1,42 @@
+import {Button, Card, Container, Row} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
+import {faEnvelope} from "@fortawesome/free-regular-svg-icons";
+import React from "react";
+import PropTypes from "prop-types";
+
+function ContactCard(props) {
+
+    ContactCard.propTypes = {
+        photo: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        gh_link: PropTypes.string,
+        email: PropTypes.string.isRequired,
+    };
+
+    return (
+        <>
+            <Card style={{width: '20rem'}}>
+                <Card.Img variant="top" style={{height: "20rem"}}
+                          src={props.photo}/>
+                <Card.Body>
+                    <Card.Title>{props.name}</Card.Title>
+                    <Card.Text>
+                        <Container>
+                            <Row className={"justify-content-around mt-4"}>
+                                {props.gh_link && <a href={props.gh_link}>
+                                    <FontAwesomeIcon icon={faGithub} style={{fontSize: "24pt"}}/>
+                                </a>}
+                                <a href={"mailto:" + props.email}>
+                                    <FontAwesomeIcon icon={faEnvelope} style={{fontSize: "24pt"}}/>
+                                </a>
+                            </Row>
+                        </Container>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </>
+    );
+}
+
+export default ContactCard;
--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,65 +1,33 @@
 import React from "react";
 import {Card, Col, Container, Image, Row} from "react-bootstrap";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faGithub} from "@fortawesome/free-brands-svg-icons";
-import {faMailBulk} from "@fortawesome/free-solid-svg-icons";
+import ContactCard from "../components/ContactCard";
 
 const Contact = () => {
+
     return (
         <>
             <Container className="mt-3">
                 <Row className={"justify-content-around mt-3"}>
-                    <Card style={{width: '20rem'}}>
-                        <Card.Img variant="top" style={{height: "20rem"}}
-                                  src="https://photos.cri.epita.fr/augustin.begue"/>
-                        <Card.Body>
-                            <Card.Title>Augustin Begue</Card.Title>
-                            <Card.Text>
-                                <p>augustin.begue@epita.fr</p>
-                                <p>github.com/Tagueo</p>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    <Card style={{width: '20rem'}}>
-                        <Card.Img variant="top" style={{height: "20rem"}}
-                                  src="https://avatars.githubusercontent.com/u/45126377"/>
-                        <Card.Body>
-                            <Card.Title>François Houssin</Card.Title>
-                            <Card.Text>
-                                <FontAwesomeIcon icon={faGithub} size={20}/>
-                                <FontAwesomeIcon icon={faMailBulk}/>
-                                <p>fransoishoussin@gmail.com</p>
-                                <p>github.com/Difrancium/</p>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+                    <ContactCard name={"Augustin Begue"} email={"augustin.begue@epita.fr"}
+                                 gh_link={"https://github.com/Tagueo"}
+                                 photo={"https://photos.cri.epita.fr/augustin.begue"}/>
+                    <ContactCard name={"François Houssin"} email={"fransoishoussin@gmail.com"}
+                                 gh_link={"https://github.com/Difrancium/"}
+                                 photo={"https://avatars.githubusercontent.com/u/45126377"}/>
                 </Row>
                 <Row className={"justify-content-around mt-3"}>
-                    <Card style={{width: '20rem'}}>
-                    <Card.Img variant="top" style={{height: "20rem"}}
-                              src="https://photos.cri.epita.fr/baptiste.fontaine"/>
-                    <Card.Body>
-                        <Card.Title>Baptiste Fontaine</Card.Title>
-                        <Card.Text>
-                            <p>baptiste.fontaine@epita.fr</p>
-                            <p>github.com/Hypaxba</p>
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-                    <Card style={{width: '20rem'}}>
-                        <Card.Img variant="top" style={{height: "20rem"}}
-                                  src="https://i.ibb.co/TmtDx5f/Screenshot-2021-01-28-175643.png"/>
-                        <Card.Body>
-                            <Card.Title>Paul Spielmann</Card.Title>
-                            <Card.Text>
-                                <p>paul.spielmann@epita.fr</p>
-                                <p>github.com/paul-spl</p>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+                    <ContactCard name={"Baptiste Fontaine"} email={"baptiste.fontaine@epita.fr"}
+                                 gh_link={"https://github.com/Tagueo"}
+                                 photo={"https://photos.cri.epita.fr/baptiste.fontaine"}/>
+                    <ContactCard name={"Paul Spielmann"} email={"paul.spielmann@epita.fr"}
+                                 gh_link={"https://github.com/paul-spl"}
+                                 photo={"https://i.ibb.co/TmtDx5f/Screenshot-2021-01-28-175643.png"}/>
+
                 </Row>
             </Container>
         </>
+        //This link for Paul's photo might expire one day, I don't know. In case you don't have picture look for
+        //this first.
     );
 };
 
