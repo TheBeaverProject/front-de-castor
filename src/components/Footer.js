e9589ad294abe6eea7e2a5e0dcccf79c0a209036
Simon Scatton <simon.scatton@epita.fr>
2021-01-29 22:19:30 +0100

    Added dummy links and a copyright to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,23 @@
 import React from "react";
-import {Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {Nav, Navbar} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faInstagram, faTwitter} from "@fortawesome/free-brands-svg-icons";
+import {Link} from "react-router-dom";
+import {faHeart} from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" style={{position: "relative", bottom: 0}}>
-            <Nav className="mr-auto">
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
             <Nav>
-                <Nav.Link href="#deets">More deets</Nav.Link>
-                <Nav.Link eventKey={2} href="#memes">
-                    Dank memes
-                </Nav.Link>
+                <Navbar.Text>
+                    Made with <FontAwesomeIcon icon={faHeart} style={{color: "red"}}/> by <a
+                    href="https://github.com/TheBeaverProject/">The Beaver Project</a>
+                </Navbar.Text>
             </Nav>
+            <Navbar.Collapse className="justify-content-end">
+                <Nav.Link href="https://twitter.com"><FontAwesomeIcon icon={faTwitter}/></Nav.Link>
+                <Nav.Link href="https://twitter.com"><FontAwesomeIcon icon={faInstagram}/></Nav.Link>
+            </Navbar.Collapse>
         </Navbar>
     )
 };
