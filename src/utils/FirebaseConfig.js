eebd542b436049071399766679aa62ef38a4b1e3
Simon Scatton <simon.scatton@epita.fr>
2021-02-05 14:41:47 +0100

    Added api key in clear in FirebaseConfig.js

--- src/utils/FirebaseConfig.js
+++ src/utils/FirebaseConfig.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSENGIN_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey: "AIzaSyDVEizX_DZkdCWYht3c7i83z6WbMBgewdU",
+    authDomain: "beaver-ea0ea.firebaseapp.com",
+    projectId: "beaver-ea0ea",
+    storageBucket: "beaver-ea0ea.appspot.com",
+    messagingSenderId: "210888667038",
+    appId: "1:210888667038:web:d552d573e5fe6f1af8f08f",
+    measurementId: "G-RK2L5R4XH0"
 };
