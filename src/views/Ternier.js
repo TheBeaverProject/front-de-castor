cbd9007fdfdcc15073824e48576f2e13d7068cb5
Augustin <augustin@begue.cc>
2021-06-17 20:36:52 +0200

    Ternier easter egg

---
+++ src/views/Ternier.js
@@ -0,0 +1,9 @@
+import { Container } from "react-bootstrap";
+
+const Ternier = () => {
+    return (<Container>
+        <img src="https://photos.cri.epita.fr/ian.ternier" style={{ width: '100%' }} alt="Mr Ternier"></img>
+    </Container>)
+}
+
+export default Ternier;
